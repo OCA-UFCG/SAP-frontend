c71b4a84f04de65a1852d92d9099adb94c39fcfa
analarissac <analarissacostas@gmail.com>
2026-02-13 19:01:10 -0300

    style: adjust spacing and apply responsive layout to partners section

---
+++ src/components/PartnersSection/PartnersSection.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export function PartnersSection() {
+  return (
+    <section className="w-full bg-[#F6F7F6]">
+      <div className="max-w-[1440px] mx-auto px-6 md:px-20 py-12 flex flex-col md:min-h-[328px]">
+
+        <div className="flex flex-col gap-6 w-full max-w-5xl">
+          <h2 className="text-2xl md:text-3xl font-semibold text-[#292829]">
+            Parceiros
+          </h2>
+
+          <p className="text-base leading-relaxed text-[#292829]">
+            Conheça nossos parceiros aos quais em colaboração conosco nos apoiam no desenvolvimento do Sistema de Alerta Precoce.
+          </p>
+        </div>
+
+        <div className="flex flex-wrap items-end gap-6 mt-8 md:mt-10 lg:mt-12">
+          <img src="/partners/oca.png" className="h-[78px] w-auto object-contain" />
+          <img src="/partners/insa.png" className="h-[79px] w-auto object-contain" />
+          <img src="/partners/mma.png" className="h-[78px] w-auto object-contain" />
+          <img src="/partners/bndes.png" className="h-[78px] w-auto object-contain" />
+          <img src="/partners/ufcg.png" className="h-[78px] w-auto object-contain" />
+        </div>
+
+      </div>
+    </section>
+
+
+
+
+
+  );
+}
--- src/stories/PartinersSection.stories.ts
+++ src/stories/PartinersSection.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/nextjs";
 
 
-import { PartnersSection } from '../components/PartinersSection/PartinersSection';
+import { PartnersSection } from '../components/PartnersSection/PartnersSection';
 
 const meta: Meta<typeof PartnersSection> = {
   title: 'Sections/PartnersSection',
