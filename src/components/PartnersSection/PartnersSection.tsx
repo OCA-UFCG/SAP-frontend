160f3490cb857b6ed16398a6e80911390ff9fd2a
analarissac <analarissacostas@gmail.com>
2026-02-19 17:43:25 -0300

    style: center content on mobile

--- src/components/PartnersSection/PartnersSection.tsx
+++ src/components/PartnersSection/PartnersSection.tsx
@@ -5,17 +5,19 @@ export function PartnersSection() {
     <section className="w-full bg-[#F6F7F6]">
       <div className="max-w-[1440px] mx-auto px-6 md:px-20 py-12 flex flex-col md:min-h-[328px]">
 
-        <div className="flex flex-col gap-6 w-full max-w-5xl">
+        {/* Texto */}
+        <div className="flex flex-col gap-6 w-full max-w-5xl text-center md:text-left items-center md:items-start">
           <h2 className="text-2xl md:text-3xl font-semibold text-[#292829]">
             Parceiros
           </h2>
 
-          <p className="text-base leading-relaxed text-[#292829]">
+          <p className="text-base leading-relaxed text-[#292829] max-w-2xl">
             Conheça nossos parceiros aos quais em colaboração conosco nos apoiam no desenvolvimento do Sistema de Alerta Precoce.
           </p>
         </div>
 
-        <div className="flex flex-wrap items-end gap-6 mt-8 md:mt-10 lg:mt-12">
+        {/* Logos */}
+        <div className="flex flex-wrap justify-center md:justify-start items-center gap-6 mt-8 md:mt-10 lg:mt-12">
           <img src="/partners/oca.png" className="h-[78px] w-auto object-contain" />
           <img src="/partners/insa.png" className="h-[79px] w-auto object-contain" />
           <img src="/partners/mma.png" className="h-[78px] w-auto object-contain" />
@@ -25,10 +27,5 @@ export function PartnersSection() {
 
       </div>
     </section>
-
-
-
-
-
   );
 }
