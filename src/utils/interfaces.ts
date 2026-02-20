b78d191655af4a822fa18daf91e7bf14f2e74334
Raína Souza <raiina.py@gmail.com>
2026-02-20 15:23:50 -0300

    feat: Contentful's configuration

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,17 @@
+import { Document } from "@contentful/rich-text-types";
+
+export interface AboutSectionI {
+  sys: {
+    id: string;
+  };
+  title: string;
+  text: {
+    json: Document;
+  };
+  image: {
+    url: string;
+    title: string;
+    width: number;
+    height: number;
+  };
+}
