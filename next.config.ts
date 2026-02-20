e997e0cc5f6bba1d68ad4fea7d4b5d24b26e2236
Gustavo <luis.gustavo.paiva.alves@ccc.ufcg.edu.br>
2026-02-20 15:34:11 -0300

    feat: add complete homepage banner

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //this makes the Next.js know what domain it could trust!!
   images: {
     remotePatterns: [
       {
--- src/components/MainBanner/MainBanner.tsx
+++ src/components/MainBanner/MainBanner.tsx
@@ -1,17 +1,25 @@
+import { IMainBanner } from "@/utils/interfaces";
 import Image from "next/image";
 
-export function MainBanner() {
+interface MainBannerProps {
+  data: IMainBanner;
+}
+
+export function MainBanner({ data }: MainBannerProps) {
+  if (!data) return null;
+
+  const { title, subtitle, buttonLabel, buttonUrl, image } = data;
   return (
-    <section className="relative w-full max-w-[1440px] mx-auto h-auto lg:h-[492px] pt-[48px] lg:pt-[64px] pb-[48px] lg:pb-[64px] px-6 lg:px-[80px] overflow-hidden">
+    <section className="relative w-full h-[492px] pt-[48px] lg:pt-[64px] pb-[48px] lg:pb-[64px] px-6 lg:px-[80px] overflow-hidden">
       <div className="absolute inset-0 w-full h-full z-0 overflow-hidden">
         <Image
-          src="/banner.png"
+          src={image.url}
           alt="Sistema de Alerta Precoce"
           fill
           priority
           unoptimized 
           className="object-cover object-center"
-        />
+        />  
 
         <div 
           className="absolute inset-0 z-10"
@@ -21,13 +29,13 @@ export function MainBanner() {
         />
       </div>
 
-      <div className="absolute opacity-[0.05] right-[55%] top-[-30%]">
+      <div className="absolute opacity-[0.05] left-0 -translate-x-[38%] -translate-y-[32%] pointer-events-none">
         <Image
-          src="/solar_icon.png" 
+          src="/solar_icon.png"
           alt=""
-          width={750} 
+          width={750}
           height={750}
-          className="max-w-none w-[800px] lg:w-[1000px] h-auto"
+          className="max-w-none w-[800px] lg:w-[1000px] h-auto scale-y-75 scale-x-90"
         />
       </div>
 
@@ -35,22 +43,21 @@ export function MainBanner() {
         <div className="w-full">
           <div className="flex flex-col gap-[24px] max-w-[800px] items-start text-left">
             <h1 className="font-open font-[700] text-white text-3xl md:text-5xl lg:text-[64px] leading-tight drop-shadow-sm">
-              Sistema de Alerta Precoce de Seca e Desertificação
+              {title}
             </h1>
 
             <p className="font-open font-[400] text-white text-base md:text-[16px] leading-relaxed max-w-[600px]">
-              O SAP integra dados climáticos, ambientais e socioeconômicos para monitorar, analisar e antecipar riscos de seca e desertificação, 
-              apoiando decisões estratégicas e ações de resposta.
+             {subtitle}
             </p>
 
-            <button
-              type="button"
-              className="mt-4 flex items-center justify-center w-full md:w-[302px] h-[40px] px-4 py-2 rounded-[8px] bg-[#989F43] hover:bg-[#5B612A] text-white transition-all duration-200 shadow-md active:scale-95"
+            <a
+              href={buttonUrl}
+              className="mt-4 flex items-center justify-center w-full md:w-[302px] h-[40px] px-4 py-2 rounded-[8px] bg-[#989F43] hover:bg-[#5B612A] text-white transition-all duration-200 shadow-md"
             >
               <span className="font-open text-[14px] font-[500] leading-[24px] tracking-normal">
-                Explorar o Mapa Interativo
+                {buttonLabel}
               </span>
-            </button>
+            </a>
           </div>
         </div>
       </div>
