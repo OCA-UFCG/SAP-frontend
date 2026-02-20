f670a9672b0ceac75dc9228969f24c5d26aadf7e
Raína Souza <raiina.py@gmail.com>
2026-02-20 16:55:12 -0300

    refactor(homepage): data fetching function and query

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,14 +1,14 @@
 import { getContent } from "./contentful";
 import { AboutSectionI } from "./interfaces";
-import { GET_SOBRE_NOS_HOMEPAGE } from "./queries";
+import { GET_HOME_PAGE } from "./queries";
 
 interface AboutContent {
   aboutCollection: { items: AboutSectionI[] };
 }
 
-export const getAboutSectionData = async (): Promise<AboutSectionI | null> => {
+export const getHomePageContent = async (): Promise<AboutSectionI | null> => {
   try {
-    const { aboutCollection } = await getContent<AboutContent>(GET_SOBRE_NOS_HOMEPAGE);
+    const { aboutCollection } = await getContent<AboutContent>(GET_HOME_PAGE);
     return aboutCollection.items[0] ?? null;
   } catch (error) {
     console.error("Erro ao buscar dados do Contentful:", error);
