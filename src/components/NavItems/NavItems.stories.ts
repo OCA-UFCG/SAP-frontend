9897c557ccf7dde63bb89c10212e457a3b991721
Paulo <paulo.henrique.lunguinho.santos@ccc.ufcg.edu.br>
2026-02-23 15:26:18 -0300

    test: config storybook stories to components

--- src/components/NavItems/NavItems.stories.ts
+++ src/components/NavItems/NavItems.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 import { NavItems } from './NavItems';
+import { ISections } from '@/utils/interfaces';
 
 const meta: Meta<typeof NavItems> = {
   title: 'Layout/NavItems',
@@ -15,40 +16,39 @@ const meta: Meta<typeof NavItems> = {
   },
 };
 
+const content: ISections = {
+  'home-section': {
+    id: '1',
+    name: 'Home',
+    path: '/',
+    appears: true,
+  },
+  'map-section': {
+    id: '2',
+    name: 'Mapa',
+    path: '/mock',
+    appears: true,
+  },
+  'about-section': {
+    id: '3',
+    name: 'Sobre o Sap',
+    path: '/mock',
+    appears: true,
+  },
+  'contact-section': {
+    id: '4',
+    name: 'Contatos',
+    path: '/mock',
+    appears: true,
+  },
+};
+
 export default meta;
 type Story = StoryObj<typeof NavItems>;
 
-// 1. Layout para o Header (Horizontal)
-export const DesktopHeader: Story = {
+export const DesktopNavItems: Story = {
   args: {
     className: 'flex flex-row items-center gap-[24px]',
-  },
-};
-
-// 2. Layout para Menu Lateral ou Mobile (Vertical)
-export const MobileDrawer: Story = {
-  args: {
-    className: 'flex flex-col gap-2 w-full max-w-[250px] p-4 bg-slate-50 rounded-lg',
-  },
-  parameters: {
-    nextjs: {
-      navigation: {
-        pathname: '/products', // Simula o estado ativo em uma rota específica
-      },
-    },
-  },
-};
-
-// 3. Visualização com o Link "Sobre Nós" ativo
-export const ActiveRoute: Story = {
-  args: {
-    className: 'flex flex-row gap-4',
-  },
-  parameters: {
-    nextjs: {
-      navigation: {
-        pathname: '/about',
-      },
-    },
+    content: Object.values(content),
   },
 };
