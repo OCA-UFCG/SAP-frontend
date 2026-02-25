5e365772659f131c1a048f1682d38c109c339f6d
nicolaskayamori <nicolaskayamori@gmail.com>
2026-02-25 13:40:03 -0300

    conclude footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,6 @@ import { Icon } from "@/components/Icon/Icon";
 import { channels } from "@/utils/constants";
 import { sortContentByDesiredOrder } from "@/utils/functions";
 
-
 const Footer = ({ content }: { content: FooterI[] }) => {
   const mainPages = sortContentByDesiredOrder<FooterI>(content, [
     "home",
@@ -28,14 +27,15 @@ const Footer = ({ content }: { content: FooterI[] }) => {
   return (
 
     <footer className="w-full bg-[#989F43]">
-      <div className="flex flex-col lg:flex-row justify-around items-top max-w-360 px-20 py-16 gap-4 w-full mx-auto">
-        <div className="flex justify-center w-full lg:w-auto">
-          <Icon
-            id="logo-sap"
-            className="block"
+      <div className="flex flex-col lg:flex-row justify-between items-start max-w-360 px-14 py-14 gap-4 w-full mx-auto">
+        <div className="flex justify-start w-full lg:w-auto">
+          <img
+            src="/Group 10.svg"
+            alt="SAP"
+            className="h-32 w-auto -mt-4"
           />
         </div>
-        <div className="hidden lg:flex gap-12 w-full lg:w-auto px-4">
+        <div className="hidden lg:flex gap-12 w-full lg:w-auto px-4 items-start">
           <div className="flex flex-col items-center lg:items-start gap-3">
             {mainPages.map(({ id, path, name }) => (
               <a
@@ -47,7 +47,7 @@ const Footer = ({ content }: { content: FooterI[] }) => {
               </a>
             ))}
           </div>
-          <div className="flex gap-3">
+          <div className="flex gap-20">
             {splitColumns(macroThemes, 6).map((columnItems, columnIndex) => (
               <div
                 key={`column-${columnIndex}`}
@@ -67,7 +67,7 @@ const Footer = ({ content }: { content: FooterI[] }) => {
           </div>
         </div>
 
-        <div className="flex flex-col justify-between lg:items-start w-full lg:w-auto gap-4">
+        <div className="flex flex-col justify-between lg:items-start lg:ml-auto w-full lg:w-auto gap-4">
           <div className="flex flex-col lg:items-start gap-4">
             <p className="hidden lg:flex text-white font-semibold text-2xl">
               Redes sociais e contatos
@@ -85,21 +85,17 @@ const Footer = ({ content }: { content: FooterI[] }) => {
               ))}
             </div>
             <p className="text-white font-medium text-sm text-center">
-              sap@oca.com
+              emailsap@oca.com
             </p>
           </div>
-          <div className="flex flex-wrap gap-4 justify-center lg:justify-start px-4 lg:px-0 max-w-2xl mx-auto">
+          <div className="flex flex-wrap gap-4 justify-center lg:justify-start px-4 lg:px-0 max-w-2xl mx-auto lg:mx-0">
             <Icon
               id="logo-gov"
-              width={198}
-              height={94}
-              className="w-32 h-15 sm:w-49.5 sm:h-23.5"
+              className="w-16 h-10 sm:w-32 sm:h-16"
             />
             <Icon
-              id="logo-sap"
-              width={198}
-              height={94}
-              className="w-32 h-15 sm:w-49.5 sm:h-23.5"
+              id="logo-mma"
+              className="w-16 h-10 sm:w-32 sm:h-16"
             />{" "}
           </div>
         </div>
