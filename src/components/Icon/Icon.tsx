bfcbef3f295855c5e93382f04381cbc43c06ed02
nicolaskayamori <nicolaskayamori@gmail.com>
2026-02-25 13:40:03 -0300

    conclude footer component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -3,24 +3,18 @@ export const Icon = ({
   size,
   width,
   height,
-  onClick,
   ...props
 }: {
   id: string;
   size?: number;
   width?: number;
   height?: number;
-  props?: object;
-  onClick?: (arg0: any) => void;
-  className?: string;
-}) => {
+} & React.SVGProps<SVGSVGElement>) => {
   return (
     <svg
       {...props}
-      onClick={onClick}
-      width={size ? size : width}
-      height={size ? size : height}
-      className={props.className}
+      width={size ?? width}
+      height={size ?? height}
     >
       <use href={`/sprite.svg#${id}`} />
     </svg>
