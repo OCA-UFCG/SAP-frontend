1073eb9ed93f410b0e05e3bfac89c149fdf70d32
Paulo <paulo.henrique.lunguinho.santos@ccc.ufcg.edu.br>
2026-02-25 15:22:56 -0300

    refactor: create sprites.svg and adjust sidebar height

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,23 +7,21 @@ import { ISection } from '@/utils/interfaces';
 
 export const Header = ({ content }: { content: ISection[] }) => {
   return (
-    <nav className="fixed top-0 z-50 w-full h-[66px] bg-[#F6F3F0]  border-b-[2px] border-[#777E32]">
-      <div className='relative mx-auto max-w-[1440px] h-full'>
-        <div className="absolute flex items-center justify-center top-0 left-0 z-1001 h-[82px] bg-[#989F43] w-[176px]">
+    <nav className="fixed top-0 z-50 w-full h-16.5 bg-[#F6F3F0]  border-b-2 border-[#777E32]">
+      <div className='relative mx-auto max-w-360 h-full'>
+        <div className="absolute flex items-center justify-center top-0 left-0 z-1001 h-20.5 bg-[#989F43] w-44">
           <Link href="/" className="relative z-10">
             <Icon id="logo" width={128} height={72} />
           </Link>
         </div>
 
         <svg
-          id="triangle"
-          viewBox="0 0 100 100"
           className="absolute top-[66px] left-[163px] z-1000 w-[26px] h-[19px] fill-[#21240F] rotate-180"
         >
-          <polygon points="50 15, 100 100, 0 100" />
+          <use href="./sprites.svg#icon-triangle" />
         </svg>
 
-        <div className="w-full flex h-full items-center justify-end pr-[80px]">
+        <div className="w-full flex h-full items-center justify-end pr-20">
           <div className="hidden xl:block">
             <NavItems
               className="flex gap-x-[24px]"
