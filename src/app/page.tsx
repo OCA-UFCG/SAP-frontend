b3f5ebd14dbc37a2acaf408d8af1ad8bad2793e5
Raína Souza <raiina.py@gmail.com>
2026-02-26 10:52:22 -0300

    feat: add AlertTiers story

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { getContent } from "../utils/contentful";
 import { GET_HOME_PAGE } from "../utils/queries";
 import { AboutSectionI, StatusItemI, IMainBanner } from "../utils/interfaces";
 import { AboutSection } from "../components/AboutSection/AboutSection";
-import { AlertTiers } from "../components/AlertTiers/AlertTiers";
 import { MainBanner } from "../components/MainBanner/MainBanner";
 
 interface HomeContent {
@@ -34,61 +33,12 @@ export default async function Home() {
   const mainBannerData = data.bannerCollection?.items[0];
   const aboutData = data.aboutCollection?.items[0];
 
-  const items: StatusItemI[] = [
-    {
-      id: "1",
-      label: "Sem seca",
-      value: 43.3,
-      color: "#F0F0D7",
-      textColor: "#292829"
-    },
-    {
-      id: "2",
-      label: "Observação (Watch)",
-      value: 24.5,
-      color: "#FECB89",
-      textColor: "#292829"
-
-    },
-    {
-      id: "3",
-      label: "Atenção (Warning)",
-      value: 6,
-      color: "#FC8F23",
-      textColor: "#292829"
-    },
-    {
-      id: "4",
-      label: "Alerta (Alert)",
-      value: 13.2,
-      color: "#B52C08",
-      textColor: "#F8F7F8"
-
-    },
-    {
-      id: "6",
-      label: "Recuperação Total (Full Recovery)",
-      value: 13,
-      color: "#B4BA61",
-      textColor: "#F8F7F8"
-
-    },
-        {
-      id: "7",
-      label: "Recuperação Parcial (Partial Recovery)",
-      value: 13,
-      color: "#5B612A",
-      textColor: "#F8F7F8"
-
-    },
-  ];
 
   return (
     <div className="flex min-h-screen flex-col">
       {mainBannerData && <MainBanner data={mainBannerData} />}
       {aboutData && <AboutSection content={aboutData} />}
 
-      <AlertTiers items={items}></AlertTiers>
     </div>
   );
 }
