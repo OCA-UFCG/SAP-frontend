c4058c5c39c3088b78c4acdc997d6cfdc4933581
Raína Souza <raiina.py@gmail.com>
2026-02-27 20:04:50 -0300

    refactor: styling

--- src/components/StatusRow/StatusRow.tsx
+++ src/components/StatusRow/StatusRow.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const StatusRow = ({ item }: Props) => {
   return (
-    <div className="flex justify-between items-center h-[40px] pl-2 pr-0 bg-white rounded-[8px] gap-4 w-[432px]">
+    <div className="flex justify-between items-center min-h-10 pl-2 pr-0 bg-white rounded-[8px] gap-4 w-[432px]">
 
       <div className="flex items-center gap-2">
         <input
@@ -24,7 +24,7 @@ export const StatusRow = ({ item }: Props) => {
 
       <div
         style={{ backgroundColor: item.color, color: item.textColor ?? '#292829' }}
-        className="flex items-center justify-center w-[68px] h-[40px] rounded-[8px] text-[16px] font-semibold font-['Open_Sans']"
+        className="flex items-center justify-center min-w-17 min-h-10 rounded-lg text-[16px] font-semibold font-['Open_Sans']"      
       >
         {item.value}%
       </div>
