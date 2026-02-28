adeeca663dd711a23e3e3caf9f6c9b6a521a8dfa
marcelodelazari <marceloalvesdelazari@gmail.com>
2026-02-28 19:55:47 -0300

    Reduce whitespace before footer on contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import ContactSection from "@/components/ContactSection/ContactSection";
 
 export default function ContactPage() {
   return (
-    <main className="min-h-screen w-full flex justify-center bg-white">
+    <main className="w-full flex justify-center bg-white">
       <ContactSection />
     </main>
   );
