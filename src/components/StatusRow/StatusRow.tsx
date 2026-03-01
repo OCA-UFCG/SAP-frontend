20340b2559174dda9078ffac4f08adbfab34f199
marcelodelazari <marceloalvesdelazari@gmail.com>
2026-03-01 20:23:27 -0300

    quick fix: remove checkbox input from chart

--- src/components/StatusRow/StatusRow.tsx
+++ src/components/StatusRow/StatusRow.tsx
@@ -8,35 +8,27 @@ type Props = {
 };
 
 export const StatusRow = ({ item, onToggle }: Props) => {
-
   const handleToggle = (e: ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
     if (onToggle) {
-      onToggle(item.id, e.target.checked)
+      onToggle(item.id, e.target.checked);
     }
-  }
+  };
 
   return (
     <div className="flex justify-between items-center min-h-10 pl-2 pr-0 bg-white rounded-lg gap-4 w-full">
       <div className="flex items-center gap-2">
-        <input
-          type="checkbox"
-          checked={item.checked}
-           onChange={handleToggle}         
-          readOnly
-          className="w-4 h-4 rounded-sm accent-[#5B612A]"
-        />
-        <span className="text-[14px] font-semibold">
-          {item.label}
-        </span>
+        <span className="text-[14px] font-semibold">{item.label}</span>
       </div>
 
       <div
-        style={{ backgroundColor: item.color, color: getContrastTextColor(item.color) }}
+        style={{
+          backgroundColor: item.color,
+          color: getContrastTextColor(item.color),
+        }}
         className="flex items-center justify-center min-w-17 min-h-10 rounded-lg text-[16px] font-semibold font-['Open_Sans']"
       >
         {item.value}%
       </div>
-
     </div>
   );
 };
