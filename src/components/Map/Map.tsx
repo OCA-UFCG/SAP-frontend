5dd73ce9d153a13e391a4900627f79307f0aff5b
Gustavo <luis.gustavo.paiva.alves@ccc.ufcg.edu.br>
2026-03-26 18:11:48 -0300

    feat: only show state borders conditionally

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -220,20 +220,22 @@ const Map = ({
           url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
           attribution="&copy; Esri"
         />
-        <GeoJSON
-          data={dadosCDI as FeatureCollection}
-          key={`cdi-layer`}
-          style={vectorStyle}
-        />
-      { showStatesBorder && (
-        <GeoJSON
-          data={geometria as FeatureCollection}
-          onEachFeature={onEachFeature}
-          // Note: key should be unique to re-render if geometry changes
-          key={`${estadoSelecionado}`}
-          style={defaultStyle}
-        />
-      )}
+        {dadosCDI && (
+          <GeoJSON
+            data={dadosCDI as FeatureCollection}
+            key={`cdi-layer-${!!dadosCDI}`}
+            style={vectorStyle}
+          />
+        )}
+        {showStatesBorder && (
+          <GeoJSON
+            data={geometria as FeatureCollection}
+            onEachFeature={onEachFeature}
+            // Note: key should be unique to re-render if geometry changes
+            key={`${estadoSelecionado}`}
+            style={defaultStyle}
+          />
+        )}
       </MapContainer>
     </div>
   );
