bec9c5350dd65fbb6ba61d0207e1c87e7e31091a
nicolaskayamori <nicolaskayamori@gmail.com>
2026-04-24 09:33:09 -0300

    adds base component for temporal vision

---
+++ src/components/TemporalVision/TemporalVision.tsx
@@ -0,0 +1,20 @@
+export function TemporalVision() {
+    return (
+        <>
+            <div className="h-6 w-[132px]">
+                {/* Aqui fica o titulo da secao de visao temporal. */}
+                <h2 className="font-['Open-Sans'] font-semibold text-[18px] leading-[24px]">
+                    Visão temporal
+                </h2>
+            </div>
+
+            <div className="min-h-[54px] rounded-lg">
+                {/* Aqui fica o texto de contexto temporal da analise. */}
+            </div>
+
+            <div className="min-h-[293px] rounded-lg bg-white">
+                {/* Aqui fica a visualizacao grafica ou imagem da serie temporal. */}
+            </div>
+        </>
+    )
+}
