1fcb374e4db32492d08a099bff48940166444006
Gustavo <luis.gustavo.paiva.alves@ccc.ufcg.edu.br>
2026-06-18 15:03:11 -0300

    feat: Added middleware to handle automatic browser language detection and redirects

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./translations/routing";
+
+export default createMiddleware(routing);
+
+export const config = {
+  matcher: [
+    "/((?!api|_next|_vercel|.*\\..*).*)",
+    "/",
+    "/(pt|en|es)/:path*",
+  ],
+};
